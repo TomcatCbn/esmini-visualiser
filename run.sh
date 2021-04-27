d622badea392fe85eb402b2f565412601f6b9f8e
Chenboning <Boning.Chen@mobilityasia.com.cn>
2021-04-27 19:12:47 +0800

    @dev adjust

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 Xvfb :1 -screen 0 ${RESOLUTION}x16 &> xvfb.log  &
 
-cd /root/esmini/build/EnvironmentSimulator/EgoSimulator/
+cd /root/esmini-self/build/EnvironmentSimulator/Applications/esmini
 target=$(echo $SCENARIO | sed 's/\(.*\).xosc/\1\.gif/')
-timeout $LENGTH ./EgoSimulator --window 0 0 ${RESOLUTION//x/ } --osc "/scenarios/${SCENARIO}" --ext_control off &
+timeout $LENGTH ./esmini --window 0 0 ${RESOLUTION//x/ } --osc "/scenarios/${SCENARIO}" --disable_controllers &
 avconv -f x11grab -s $RESOLUTION -r $FRAMERATE -t $LENGTH -y -i :1.0 "/scenarios/${target}"
